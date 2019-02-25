b9e3ebd9f9b9d614b9c0a0dda87693289fbec4fc
Lucas Eckman <lucas.eckman@gmail.com>
2019-02-25 14:35:20 -0500

    mongoose connected

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 
+import models, { connectDb } from "./models";
+
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+connectDb().then(async () => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`)
+  );
 });
-
-app.listen(3000, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
